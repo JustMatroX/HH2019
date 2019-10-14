9796ae1a9a0ba1568d561c857035d9d8ed899d94
ArekStecnel <arkstencel@gmail.com>
2019-10-14 23:02:34 +0200

    dodanie onclick, cssa oraz case

--- script/world.js
+++ script/world.js
@@ -3,8 +3,530 @@ let z = 1;
 
 function selectCountry(name){
     switch(name){
+        case "united_arab_emirates":
+            console.log(name)
+            break
+        case "afghanistan":
+            console.log(name)
+            break
+        case "albania":
+            console.log(name)
+            break
+        case "armenia":
+            console.log(name)
+            break
+        case "angola":
+            console.log(name)
+            break
+        case "argentina":
+            console.log(name)
+            break
+        case "austria":
+            console.log(name)
+            break
+        case "australia":
+            console.log(name)
+            break
+        case "azerbaijan":
+            console.log(name)
+            break
+        case "bosnia_and_herzegovina":
+            console.log(name)
+            break
+        case "bangladesh":
+            console.log(name)
+            break
+        case "belgium":
+            console.log(name)
+            break
+        case "burkina_faso":
+            console.log(name)
+            break
+        case "bulgaria":
+            console.log(name)
+            break
+        case "burundi":
+            console.log(name)
+            break
+        case "benin":
+            console.log(name)
+            break
+        case "brunei_darussalam":
+            console.log(name)
+            break
+        case "bolivia":
+            console.log(name)
+            break
+        case "brazil":
+            console.log(name)
+            break
+        case "bahamas":
+            console.log(name)
+            break
+        case "bhutan":
+            console.log(name)
+            break
+        case "botswana":
+            console.log(name)
+            break
+        case "belarus":
+            console.log(name)
+            break
+        case "belize":
+            console.log(name)
+            break
         case "canada":
-            console.log('Canada!')
+            console.log(name)
+            break
+        case "democratic_republic_of_congo":
+            console.log(name)
+            break
+        case "central_african_republic":
+            console.log(name)
+            break
+        case "republic_of_congo":
+            console.log(name)
+            break
+        case "switzerland":
+            console.log(name)
+            break
+        case "côte_dIvoire":
+            console.log(name)
+            break
+        case "chile":
+            console.log(name)
+            break
+        case "cameroon":
+            console.log(name)
+            break
+        case "china":
+            console.log(name)
+            break
+        case "colombia":
+            console.log(name)
+            break
+        case "costa_rica":
+            console.log(name)
+            break
+        case "cuba":
+            console.log(name)
+            break
+        case "cyprus":
+            console.log(name)
+            break
+        case "czechia":
+            console.log(name)
+            break
+        case "germany":
+            console.log(name)
+            break
+        case "djibouti":
+            console.log(name)
+            break
+        case "denmark":
+            console.log(name)
+            break
+        case "dominican_republic":
+            console.log(name)
+            break
+        case "algeria":
+            console.log(name)
+            break
+        case "ecuador":
+            console.log(name)
+            break
+        case "estonia":
+            console.log(name)
+            break
+        case "egypt":
+            console.log(name)
+            break
+        case "western_sahara":
+            console.log(name)
+            break
+        case "eritrea":
+            console.log(name)
+            break
+        case "spain":
+            console.log(name)
+            break
+        case "ethiopia":
+            console.log(name)
+            break
+        case "falkland_islands":
+            console.log(name)
+            break
+        case "finland":
+            console.log(name)
+            break
+        case "fiji":
+            console.log(name)
+            break
+        case "france":
+            console.log(name)
+            break
+        case "gabon":
+            console.log(name)
+            break
+        case "united_kingdom":
+            console.log(name)
+            break
+        case "georgia":
+            console.log(name)
+            break
+        case "french_guiana":
+            console.log(name)
+            break
+        case "ghana":
+            console.log(name)
+            break
+        case "greenland":
+            console.log(name)
+            break
+        case "gambia":
+            console.log(name)
+            break
+        case "guinea":
+            console.log(name)
+            break
+        case "equatorial_guinea":
+            console.log(name)
+            break
+        case "greece":
+            console.log(name)
+            break
+        case "guatemala":
+            console.log(name)
+            break
+        case "guinea_bissau":
+            console.log(name)
+            break
+        case "guyana":
+            console.log(name)
+            break
+        case "honduras":
+            console.log(name)
+            break
+        case "croatia":
+            console.log(name)
+            break
+        case "haiti":
+            console.log(name)
+            break
+        case "hungary":
+            console.log(name)
+            break
+        case "indonesia":
+            console.log(name)
+            break
+        case "ireland":
+            console.log(name)
+            break
+        case "israel":
+            console.log(name)
+            break
+        case "india":
+            console.log(name)
+            break
+        case "iraq":
+            console.log(name)
+            break
+        case "iran":
+            console.log(name)
+            break
+        case "iceland":
+            console.log(name)
+            break
+        case "italy":
+            console.log(name)
+            break
+        case "jamaica":
+            console.log(name)
+            break
+        case "jordan":
+            console.log(name)
+            break
+        case "japan":
+            console.log(name)
+            break
+        case "kenya":
+            console.log(name)
+            break
+        case "kyrgyzstan":
+            console.log(name)
+            break
+        case "cambodia":
+            console.log(name)
+            break
+        case "north_korea":
+            console.log(name)
+            break
+        case "south_korea":
+            console.log(name)
+            break
+        case "kosovo":
+            console.log(name)
+            break
+        case "kuwait":
+            console.log(name)
+            break
+        case "kazakhstan":
+            console.log(name)
+            break
+        case "lao_peoples_remocratic_republic":
+            console.log(name)
+            break
+        case "lebanon":
+            console.log(name)
+            break
+        case "sri_lanka":
+            console.log(name)
+            break
+        case "liberia":
+            console.log(name)
+            break
+        case "lesotho":
+            console.log(name)
+            break
+        case "lithuania":
+            console.log(name)
+            break
+        case "luxembourg":
+            console.log(name)
+            break
+        case "latvia":
+            console.log(name)
+            break
+        case "libya":
+            console.log(name)
+            break
+        case "morocco":
+            console.log(name)
+            break
+        case "moldova":
+            console.log(name)
+            break
+        case "montenegro":
+            console.log(name)
+            break
+        case "madagascar":
+            console.log(name)
+            break
+        case "macedonia":
+            console.log(name)
+            break
+        case "mali":
+            console.log(name)
+            break
+        case "myanmar":
+            console.log(name)
+            break
+        case "mongolia":
+            console.log(name)
+            break
+        case "mauritania":
+            console.log(name)
+            break
+        case "malawi":
+            console.log(name)
+            break
+        case "mexico":
+            console.log(name)
+            break
+        case "malaysia":
+            console.log(name)
+            break
+        case "mozambique":
+            console.log(name)
+            break
+        case "namibia":
+            console.log(name)
+            break
+        case "new_caledonia":
+            console.log(name)
+            break
+        case "niger":
+            console.log(name)
+            break
+        case "nigeria":
+            console.log(name)
+            break
+        case "nicaragua":
+            console.log(name)
+            break
+        case "netherlands":
+            console.log(name)
+            break
+        case "norway":
+            console.log(name)
+            break
+        case "nepal":
+            console.log(name)
+            break
+        case "new_zealand":
+            console.log(name)
+            break
+        case "oman":
+            console.log(name)
+            break
+        case "panama":
+            console.log(name)
+            break
+        case "peru":
+            console.log(name)
+            break
+        case "papua_new_guinea":
+            console.log(name)
+            break
+        case "philippines":
+            console.log(name)
+            break
+        case "poland":
+            console.log(name)
+            break
+        case "pakistan":
+            console.log(name)
+            break
+        case "puerto_rico":
+            console.log(name)
+            break
+        case "palestinian_territories":
+            console.log(name)
+            break
+        case "portugal":
+            console.log(name)
+            break
+        case "paraguay":
+            console.log(name)
+            break
+        case "qatar":
+            console.log(name)
+            break
+        case "romania":
+            console.log(name)
+            break
+        case "serbia":
+            console.log(name)
+            break
+        case "russia":
+            console.log(name)
+            break
+        case "rwanda":
+            console.log(name)
+            break
+        case "saudi_arabia":
+            console.log(name)
+            break
+        case "solomon_islands":
+            console.log(name)
+            break
+        case "sudan":
+            console.log(name)
+            break
+        case "sweden":
+            console.log(name)
+            break
+        case "slovenia":
+            console.log(name)
+            break
+        case "svalbard_and_jan_mayen":
+            console.log(name)
+            break
+        case "slovakia":
+            console.log(name)
+            break
+        case "sierra_leone":
+            console.log(name)
+            break
+        case "senegal":
+            console.log(name)
+            break
+        case "somalia":
+            console.log(name)
+            break
+        case "suriname":
+            console.log(name)
+            break
+        case "south_sudan":
+            console.log(name)
+            break
+        case "el_salvador":
+            console.log(name)
+            break
+        case "syria":
+            console.log(name)
+            break
+        case "swaziland":
+            console.log(name)
+            break
+        case "chad":
+            console.log(name)
+            break
+        case "togo":
+            console.log(name)
+            break
+        case "thailand":
+            console.log(name)
+            break
+        case "tajikistan":
+            console.log(name)
+            break
+        case "timor_leste":
+            console.log(name)
+            break
+        case "turkmenistan":
+            console.log(name)
+            break
+        case "tunisia":
+            console.log(name)
+            break
+        case "turkey":
+            console.log(name)
+            break
+        case "trinidad_and_tobago":
+            console.log(name)
+            break
+        case "taiwan":
+            console.log(name)
+            break
+        case "tanzania":
+            console.log(name)
+            break
+        case "ukraine":
+            console.log(name)
+            break
+        case "uganda":
+            console.log(name)
+            break
+        case "united_states":
+            console.log(name)
+            break
+        case "uruguay":
+            console.log(name)
+            break
+        case "uzbekistan":
+            console.log(name)
+            break
+        case "venezuela":
+            console.log(name)
+            break
+        case "vietnam":
+            console.log(name)
+            break
+        case "vanuatu":
+            console.log(name)
+            break
+        case "yemen":
+            console.log(name)
+            break
+        case "south_africa":
+            console.log(name)
+            break
+        case "zambia":
+            console.log(name)
+            break
+        case "zimbabwe":
+            console.log(name)
             break
 
     }
