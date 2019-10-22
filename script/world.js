83628ba2ecf661c623e7478d6768f627a3dbbdbf
forkloo <piotrorlof.po@gmail.com>
2019-10-23 00:38:18 +0200

    poprawki w skalowaniu layoutu, dodanie granic mapy

--- script/world.js
+++ script/world.js
@@ -1,6 +1,7 @@
-let zoom = [0.6 , 0.8, 1, 1.2, 1.4, 1.6];
+let zoom = [0.6 , 0.8, 1, 1.2, 1.4, 1.6, 2.0, 2.4, 2.8, 3.2, 3.6, 4.0, 4.4, 4.8];
 let z = 1;
 let windowMode;
+let dragIter = 0;
 let mapOffset = [getCurrentMapPos("x"),getCurrentMapPos("y")];
 
 function setup(){
@@ -10,7 +11,7 @@ function setup(){
     
 }
 function draw(){
-
+    
 }
 
 function windowResized(){
@@ -25,13 +26,13 @@ function autoResize(){
         document.getElementById('mapa').style.transform = "translate( -50%, -50%) scale("+zoom[z]+")";
     }
     else if(innerWidth > 600 && innerWidth < 768){ //TABLET
-        z = 1;
+        z = 0;
         windowMode = 1
         mapOffset = [getCurrentMapPos('x'), getCurrentMapPos('y')];
         document.getElementById('mapa').style.transform = "translate( -50%, -45%) scale("+zoom[z]+")"
     }
     else if(innerWidth > 768){   //DESKTOP
-        z = 2;
+        z = 1;
         windowMode = 2
         mapOffset = [getCurrentMapPos('x'), getCurrentMapPos('y')];
         document.getElementById('mapa').style.transform = "translate( -50%, -50%) scale("+zoom[z]+")"
@@ -88,28 +89,27 @@ function zoomOut(){
  function dragMap(){
     let x = 0
     let y = 0
-    x = mouseX - pmouseX;
-    y = mouseY - pmouseY;
-    // if(mapOffset[0] <= 250 && mapOffset[0] >= -520){
-        
-    // }
-    // else if(mapOffset[0] >= 250){
-    //     mapOffset[0] = 250;
-    // }
-    // else if(mapOffset[0] <= -520){
-    //     mapOffset[0] = -520;
-    // }
+    
+    if(mapOffset[0] >= -1060 && mapOffset[0] <= 140){    //GRANICA LEWA i PRAWA
+        x = mouseX - pmouseX;
+    }
+    else if(mapOffset[0] <= -1060){ //LEWA
+        x = 1;
+    }
+    else if(mapOffset[0] >= 140){   //PRAWA
+        x = -1;
+    }
 
-    // if(mapOffset[1] <= 320 && mapOffset[1] >= -230){
-        
-    // }
-    // else if(mapOffset[1] >= 320){
-    //     mapOffset[1] = 320;
-    // }
-    // else if(mapOffset[1] <= -230){
-    //     mapOffset[1] = -230;
-    // }
-    console.log(mapOffset)
+    if(mapOffset[1] >= -720 && mapOffset[1] <= 40){     //GRANICA GÓRNA i DOLNA
+        y = mouseY - pmouseY;
+    }
+    else if(mapOffset[1] <= -720){  //GÓRNA
+        y = 1;
+    }
+    else if(mapOffset[1] >= 40){    //DOLNA
+        y = -1;
+    }
+    
     mapOffset = [mapOffset[0]+x, mapOffset[1]+y]
     document.getElementById('mapa').style.transform = "translate("+mapOffset[0]+"px,"+mapOffset[1]+"px) scale("+zoom[z]+")";
  }
