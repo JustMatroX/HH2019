d9b31fe19a6315a663533992a7f96bdbe09fbf7d
forkloo <piotrorlof.po@gmail.com>
2019-10-24 23:35:39 +0200

    podział problemów na strony znakiem "&", css

--- script/nations.js
+++ script/nations.js
@@ -18,7 +18,8 @@ function setContent(){
             document.getElementById('title').innerHTML = newName;
 			
 			newProblems = nameTable[i]['problems'];
-			document.getElementById('data').innerHTML = newProblems;
+            divideData(newProblems);
+            chPage(0);
 			document.getElementById('svgVp').innerHTML = "<img id='svgImg' src='../svg/"+title+"Low.svg#svgView(viewBox(0,50,1250,1250))'/>"
             break;
         }
@@ -35,3 +36,31 @@ function getTitle(){
 	document.getElementById('title').innerHTML = title[0].toUpperCase()+title.substr(1);
 	return title;
 }	
+let pages = [];
+function divideData(data){
+     let charPos = []; let txtLength;
+    //znajdź wszystkie indeksy znaków przerwania//
+    for(let i = 0; i < data.length; i++){
+        if(data[i] == "&"){
+            charPos.push(i);
+        }
+    }
+    let bRead = 0; let finalChar = charPos.length;
+    //dodaj pojedynczy problem do tablicy pages//
+    for(let i = 0; i < (charPos.length+1); i++){
+        if(i == finalChar){
+            charPos.push(data.length);  //jeżeli nie ma już więcej znaków przerwania, dodaj długość danych
+        }
+        
+        pages.push(data.substr(bRead, charPos[i] - bRead /* oblicz długość tekstu */));
+        bRead = charPos[i]+1;
+        if(i != finalChar+1){
+            console.log(i)
+            document.getElementById('pages').innerHTML+="<button class='page"+i+"'onclick='chPage("+i+")'>"+(i+1)+"</button>";
+        }
+    }
+}
+let pageNum = 0;
+function chPage(n){
+    document.getElementById('dataHolder').innerHTML = pages[n]
+}
